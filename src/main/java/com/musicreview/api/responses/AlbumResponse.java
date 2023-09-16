ca9fb988a81a066dc1ac59681830441cb0380722
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-09-16 17:18:22 +0200

    changed variables order so that the json response is more readible

--- src/main/java/com/musicreview/api/responses/AlbumResponse.java
+++ src/main/java/com/musicreview/api/responses/AlbumResponse.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 @Data
 public class AlbumResponse {
-    private List<AlbumDTO> content;
     private int pageNo;
     private int pageSize;
     private long totalElements;
     private int totalPages;
     private boolean last;
+    private List<AlbumDTO> content;
 }
--- src/main/java/com/musicreview/api/responses/ReviewResponse.java
+++ src/main/java/com/musicreview/api/responses/ReviewResponse.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 @Data
 public class ReviewResponse {
-    private List<ReviewDTO> content;
     private int pageNo;
     private int pageSize;
     private long totalElements;
     private int totalPages;
     private boolean last;
+    private List<ReviewDTO> content;
 }
