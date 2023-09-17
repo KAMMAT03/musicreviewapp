72cdf021a00a743ba909fee060deaf3472fec310
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-09-17 14:49:08 +0200

    created repositories for user and role classes

---
+++ src/main/java/com/musicreview/api/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.musicreview.api.repositories;
+
+import com.musicreview.api.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
