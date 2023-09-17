40ac177fe2e777406e5c26207ce94109f054452b
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-09-17 18:11:20 +0200

    added TokenGenerator class as well as class that handles constants

---
+++ src/main/java/com/musicreview/api/security/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.musicreview.api.security;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 70000;
+    public static final String JWT_SECRET = "secret";
+}
