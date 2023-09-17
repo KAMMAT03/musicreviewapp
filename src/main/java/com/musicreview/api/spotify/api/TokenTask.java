c9d45d2803bedea63baddaea78711e71ceb5f01a
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-09-17 13:34:57 +0200

    minor temporary adjustment in TokenTask class

--- src/main/java/com/musicreview/api/spotify/api/TokenTask.java
+++ src/main/java/com/musicreview/api/spotify/api/TokenTask.java
@@ -14,6 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
+import java.util.Scanner;
 
 @Component
 public class TokenTask {
@@ -51,8 +52,10 @@ public class TokenTask {
     private static HttpURLConnection getHttpURLConnection() throws IOException {
         String endpoint = "https://accounts.spotify.com/api/token";
 
-        String clientId = "secretid";
-        String clientSecret = "secretkey";
+        Scanner scanner = new Scanner(System.in);
+
+        String clientId = scanner.nextLine();
+        String clientSecret = scanner.nextLine();
 
         URL url = new URL(endpoint);
 
